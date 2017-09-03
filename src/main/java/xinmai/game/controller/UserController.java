73f99aeb39ec1418e6fff0ac5f6e3f25727c9180
swwt <17621738274@163.com>
2017-09-03 13:04:00 +0800

    登录controller

--- src/main/java/xinmai/game/controller/UserController.java
+++ src/main/java/xinmai/game/controller/UserController.java
@@ -1,14 +1,19 @@
 package xinmai.game.controller;
 
-import javax.servlet.http.HttpServletRequest;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import xinmai.game.model.ZhangHao;
 import xinmai.game.service.IZhangHaoService;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -16,10 +21,17 @@ public class UserController {
 
     @Resource
     private IZhangHaoService zhangHaoService;
-
-    @RequestMapping("/showUser.do")
-    public void selectUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+    @ResponseBody
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public Map<String, ZhangHao> selectUser(@RequestParam("sAccount")String sAccount, @RequestParam("sPassword")String sPassword){
+        ZhangHao zhangHao = zhangHaoService.selectZhangHao(sAccount, sPassword);
+        Map<String, ZhangHao> result= new HashMap();
+        result.put("user", zhangHao);
+        if(zhangHao != null){
+            return result;
+        }else{
+            return result;
+        }
     }
 
 }
